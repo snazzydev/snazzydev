10088a974c4d7301a04daf39215459a592e1eb97
snazzydev <stankovicnino@gmail.com>
2019-05-14 04:13:01 +0200

    heade update

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+AOS.init();
+
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+        css3: true
+    });
+});

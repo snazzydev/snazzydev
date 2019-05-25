3890f824766190d3e9821ffa775680db519c9fcc
snazzydev <stankovicnino@gmail.com>
2019-05-25 23:40:21 +0200

    complete redesign purge

---
+++ test/js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+    css3: true,
+    controlArrowColor: "#fff"
+  });
+});
+
+/* https://github.com/alvarotrigo/fullPage.js#fullpagejs 
+http://www.uedsc.com/fullpage.html
+*/
+$(document).ready(function() {
+
+  console.log($(".section p").css("font-size"));
+});

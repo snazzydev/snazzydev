e4db043e3f02f99baf32a3394006ef76c2d12657
snazzydev <stankovicnino@gmail.com>
2019-05-26 23:47:47 +0200

    header update, contact section update

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-AOS.init();
-
-$(document).ready(function () {
-    $('#fullpage').fullpage({
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-        css3: true
-    });
-});
